c8aff50a61b2a3fc650097365a4cb67f03c86ad3
son2412 <ngovanson196@gmail.com>
2020-07-13 12:07:35 +0700

    add logger

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,13 +2,11 @@ import { createStore, applyMiddleware, Store } from 'redux';
 // import thunkMiddleware from 'redux-thunk';
 import rootReducers from './reducers/root.reducer';
 import createSagaMiddleware from 'redux-saga';
-// import logger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import rootSaga from './sagas/root.saga';
 
 const sagaMiddleware = createSagaMiddleware();
-const middlewares = [sagaMiddleware];
-// middlewares.push(logger);
-const store: Store = createStore(rootReducers, applyMiddleware(...middlewares));
+const store: Store = createStore(rootReducers, applyMiddleware(sagaMiddleware, createLogger()));
 sagaMiddleware.run(rootSaga);
 // store.subscribe(() => {});
 export default store;
